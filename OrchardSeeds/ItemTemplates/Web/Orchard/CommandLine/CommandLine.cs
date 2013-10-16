571a86ae29d70115a1eb67dcab930e83ed28e0b6
rijkshuisstijl <edwardkuenen@hotmail.com>
2013-10-16 14:13:33 +0200

    Commandline and permissions added

---
+++ OrchardSeeds/ItemTemplates/Web/Orchard/CommandLine/CommandLine.cs
@@ -0,0 +1,26 @@
+﻿#region Usings
+
+using Orchard.Commands;
+
+#endregion
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : DefaultOrchardCommandHandler
+    {
+
+        public $safeitemname$()
+        {
+        }
+
+        [CommandName("command1 argument1")]
+        [CommandHelp("command1 argument1\r\n\tDoes the following action")]
+        public void Argument1()
+        {
+            //Do some action
+
+            Context.Output.WriteLine(T("Action done").Text);
+        }
+
+    }
+}
---
+++ OrchardSeeds/ItemTemplates/Web/Orchard/Permissions/Permissions.cs
@@ -0,0 +1,45 @@
+﻿#region Usings
+
+using System.Collections.Generic;
+using Orchard.Environment.Extensions.Models;
+using Orchard.Security.Permissions;
+
+#endregion
+
+namespace $rootnamespace$
+{
+    public class Permissions : IPermissionProvider
+    {
+
+        public const string $safeitemname$PermissionName = "Permission Name";
+
+        public static readonly Permission $safeitemname$ = new Permission
+        {
+            Description = "Description of permission",
+            Name = $safeitemname$PermissionName
+        };
+     
+    
+        public Feature Feature { get; set; }
+
+        public IEnumerable<Permission> GetPermissions()
+        {
+            return new[] {$safeitemname$};
+        }
+
+        public IEnumerable<PermissionStereotype> GetDefaultStereotypes()
+        {
+            return new[]
+            {
+                new PermissionStereotype
+                {
+                    Name = "Administrator",
+                    Permissions = new[]
+                    {
+                        $safeitemname$
+                    }
+                }
+             };
+        }
+    }
+}

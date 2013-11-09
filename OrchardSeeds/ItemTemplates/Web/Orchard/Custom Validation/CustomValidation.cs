a9d6d3db7ea04a6d1e798f8cbc3771a9f33205dc
rijkshuisstijl <edwardkuenen@hotmail.com>
2013-11-09 15:47:27 +0100

    Upgrade Templatebuilder to 10.0.3.21

---
+++ OrchardSeeds/ItemTemplates/Web/Orchard/Custom Validation/CustomValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace $rootnamespace$
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class $safeitemname$Attribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
